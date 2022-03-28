d5a880c9f50afb443b4f4aada3cf2e40d96c81be
Vitaliy Ivanov <vitalijivanov1832@gmail.com>
2022-03-28 22:47:25 +0700

    deps not used - fix

--- src/components/app/CardItem.js
+++ src/components/app/CardItem.js
@@ -23,10 +23,8 @@ export default class CardItem extends Component {
       key={title + key}
       sx={{
         minWidth: {minWidth},
-        '& > *' : {
-          borderTopLeftRadius: 'calc(1.5em + 1.7vh)',
-          borderBottomRightRadius: 'calc(1.5em + 1.7vh)',
-        },
+        borderTopLeftRadius: 'calc(1.5em + 1.7vh)',
+        borderBottomRightRadius: 'calc(1.5em + 1.7vh)',
         '&:hover > *' : {
           transition: 'all 0.2s ease 0s',
           color: 'rgba(122, 102, 204, 1)',
@@ -36,7 +34,7 @@ export default class CardItem extends Component {
         },
         '&:hover > :nth-child(1)' : {
           borderTopLeftRadius: 'calc(0.9em + 1.7vh)',
-          borderBottomRightRadius: 'calc(0.8em + 1.7vh)'
+          
         }
       }}
       >
@@ -56,6 +54,7 @@ export default class CardItem extends Component {
           minHeight: {minHeight},
           backgroundColor: {backgroundColor},
           borderTopLeftRadius: '0em',
+          borderBottomRightRadius: 'calc(0.8em + 1.7vh)',
           fontSize: {fontSize},
           zIndex: 1,
           '&:hover' : {
@@ -72,7 +71,9 @@ export default class CardItem extends Component {
         image={image}
         sx={{
           maxHeight: {maxHeight},
-          maxWidth: {maxWidth}
+          maxWidth: {maxWidth},
+          borderTopLeftRadius: 'calc(1.5em + 1.7vh)',
+          borderBottomRightRadius: 'calc(1.5em + 1.7vh)'
         }}
         />
       </CardActionArea>
