87289ca3588b0472e7a4c0454daf5fd389b734ff
Vitaliy Ivanov <vitalijivanov1832@gmail.com>
2022-03-30 15:19:05 +0700

    bg image fix w,h

--- src/components/news/bground.js
+++ src/components/news/bground.js
@@ -6,20 +6,20 @@ import { CardMedia } from '@mui/material';
 const Bground = ({ img }) => {
   return (
     <CardMedia
-      component='img'
-      loading='eager'
-      image={img}
-      alt="bg"
-      sx={{
-        width: '100%',
-        height: 'auto',
-        minWidth: '300px',
-        top: '0',
-        left: '0', 
-        position: 'absolute', 
-        zIndex: -1
-      }}
-      />
+    component='img'
+    loading='eager'
+    image={img}
+    width='calc(1em + 1vw)'
+    height='auto'
+    alt="bg"
+    sx={{
+      minWidth: '320px',
+      top: '0',
+      left: '0', 
+      position: 'absolute', 
+      zIndex: -1
+    }}
+    />
   )
 }
 
